ba8c5b02363651fccdc00968c28a5465e0e1b839
davidhunt <c14464428@mydit.ie>
2016-11-13 16:50:28 +0000

    EditTask now implements DatePicker

--- app/src/main/java/com/example/david/myapplication/taskFunctionality.java
+++ app/src/main/java/com/example/david/myapplication/taskFunctionality.java
@@ -2,21 +2,17 @@ package com.example.david.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * Created by david on 13/11/16.
  */
 
-public class editTask extends Activity {
+public abstract class taskFunctionality extends Activity{
     Button completeButton;
 
     EditText titleBox;
@@ -24,12 +20,10 @@ public class editTask extends Activity {
     EditText listBox;
     EditText endBox;
 
-
-    public String chosenDate;
-
     DatePicker startDate;
 
-    long id;
+    public String chosenDate;
+    Task newTask;
 
 
     protected void onCreate(Bundle savedInstanceState)
@@ -45,69 +39,27 @@ public class editTask extends Activity {
         endBox=(EditText)findViewById((R.id.endDateEdit));
 
         startDate=(DatePicker)findViewById(R.id.datePicker);
-
-        Intent i = getIntent();
-
-        id= i.getLongExtra("id",-1);
-
-
-
-        retrieveData();
-
-
     }
-
 
     //is run when a user clicks the 'done' button
     public void complete(View v)
     {
+        //get the date chosen by the user
+        //String chosenDate =   startDate.getYear() + "-" + (startDate.getMonth()+1) + "-" + startDate.getDayOfMonth();
+
 
         databaseManager database = new databaseManager(this);
         database.open();
         //grab infromation from user input
-        Task updateTask = new Task(titleBox.getText().toString(), descriptionBox.getText().toString(), 0,
-             chosenDate , endBox.getText().toString());
-        //iupdate row in db
-        database.updateTask(updateTask,id);
+        newTask = new Task(titleBox.getText().toString(), descriptionBox.getText().toString(), 0,
+                chosenDate, endBox.getText().toString());
+        //insert into database
+        database.addTask(newTask);
 
         database.close();
 
         //return to main activity
         finish();
-    }
-
-    //retrieves data from the db and places into the correct fields so user can edit them
-    private void retrieveData(){
-        //will hold selected information
-        String data="";
-
-        databaseManager dbm = new databaseManager(this);
-        dbm.open();
-
-        //retrieve data from row
-        Cursor c = dbm.readTask(id);
-
-        if (c.moveToFirst()){
-            do{
-                data = c.getString(c.getColumnIndex("taskTitle"));
-                titleBox.setText(data);
-
-                data = c.getString(c.getColumnIndex("taskDescription"));
-                descriptionBox.setText(data);
-
-                data = c.getString(c.getColumnIndex("listID"));
-                listBox.setText(data);
-
-                data = c.getString(c.getColumnIndex("dueDate"));
-                endBox.setText(data);
-
-            }while(c.moveToNext());
-        }
-        c.close();
-
-        Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
-
-        dbm.close();
     }
 
     public void chooseDate(View v)
