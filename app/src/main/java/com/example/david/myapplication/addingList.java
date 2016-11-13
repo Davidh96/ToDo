7573c7e2240a8dbe879ae0b39a638773affc9515
davidhunt <c14464428@mydit.ie>
2016-11-13 20:38:24 +0000

    Created Intent to Add List Screen

---
+++ app/src/main/java/com/example/david/myapplication/addingList.java
@@ -0,0 +1,28 @@
+package com.example.david.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by david on 13/11/16.
+ */
+
+//will be used to create new list
+public class addingList extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addinglist);
+
+
+    }
+
+
+}
