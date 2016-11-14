541196eeafaa0c76a3d5477542c4059f79a7a592
davidhunt <c14464428@mydit.ie>
2016-11-14 17:11:30 +0000

    Able to delete list with long press

--- app/src/main/java/com/example/david/myapplication/taskListView.java
+++ app/src/main/java/com/example/david/myapplication/taskListView.java
@@ -21,6 +21,8 @@ public class taskListView extends ListActivity {
     public static Cursor c;
     public static SimpleCursorAdapter adapt;
 
+    databaseManager dbm;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasklistview);
@@ -29,7 +31,7 @@ public class taskListView extends ListActivity {
         //where i want to dsiplay the informtion
         int[] to =new int[] {R.id.listTitle1};
 
-        databaseManager dbm = new databaseManager(this);
+        dbm = new databaseManager(this);
         dbm.open();
         //retrieve cursor of all rows from db
         c = dbm.readLists();
@@ -38,6 +40,39 @@ public class taskListView extends ListActivity {
         //set adapter
         setListAdapter(adapt);
         dbm.close();
+
+        ListView lv = (ListView)findViewById(android.R.id.list);
+
+//        //open up screen to allow user to edit task
+//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView parent, View view, int position, long id) {
+//
+//                Intent editTask = new Intent(this,editTask.class);
+//                //editTask.putExtra("id",id);
+//                Toast.makeText(MainActivity.this, "" + id, Toast.LENGTH_SHORT).show();
+//                editTask.putExtra("id",id);
+//                startActivity(editTask);
+//            }
+//        });
+
+        //long press to delete list
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                           int pos, long id) {
+                // TODO Auto-generated method stub
+
+                dbm.open();
+                boolean deleted = dbm.deleteList((id));
+                if(deleted==true) {
+                    Toast.makeText(taskListView.this, "Deleted Task", Toast.LENGTH_SHORT).show();
+                }
+                dbm.close();
+
+                return true;
+            }
+        });
     }
 
     public void viewTask(View v){
